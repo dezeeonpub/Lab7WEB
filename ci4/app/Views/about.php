a0a8f41fdff8663c037ba72580393c9d78d8b57a
dezeeonpub <zulslol413@gmail.com>
2023-05-12 22:02:11 +0700

    lab7

---
+++ ci4/app/Views/about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?= $title; ?></title>
+</head>
+<body>
+    <?= $this->include('template/header'); ?>
+    <section id="about">
+        <div class="row">
+            <h1>HOLAA !!!</h1>
+            <p>Hello everyone My name is Muhammad Azizul Dzikri <b></b> See uuu :-) </p>
+        </div>
+    </section>
+    <?= $this->include('template/footer'); ?>
+</body>
+</html>
